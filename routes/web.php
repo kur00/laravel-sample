2a0412dbf4a5ad847bc97881f5c9bee040a92d59
fukuchi <over.y31.bsk@gmail.com>
2024-12-16 12:58:47 +0900

    feat: create company page and layout

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
-
+use App\Http\Controllers\CompanyController;
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
+
+Route::get('/companies', [CompanyController::class, 'index'])->name('companies.index');
